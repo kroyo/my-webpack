cb146c24747997e4b0372e642a9e32bfecd9699e
kroyo <kroyoshi@163.com>
2018-11-19 14:51:45 +0800

    配置项目静态资源与css

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,48 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-  entry: path.join(__dirname,'src/main.js'),
+  entry: path.join(__dirname,'src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname,'dist')
-  }
+    path: path.join(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.styl/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'stylus-loader'
+        ]
+      },
+      {
+        test: /\.(gif|jpg|jpeg|png|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1024,
+              name: '[name]-aaa.[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 }
