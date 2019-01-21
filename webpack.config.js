5e1cca246fa8644be06b7e7d479713fb0d96308c
kroyo <kroyoshi@163.com>
2019-01-21 18:08:36 +0800

    css文件引入，hot更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,72 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    // app: './src/index.js',
+    // print: './src/print.js'
+    app: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    // 自动清理文件
+    new CleanWebpackPlugin(['dist']),
+    // HtmlWebpackPlugin 默认生成 index.html 文件
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    }),
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        // css样式
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        // 图片
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        // 字体
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        // 导入csv
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        // 导入xml
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      }
+    ]
   }
 }
