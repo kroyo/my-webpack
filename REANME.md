4715056cf6ca0b4f29e58397ccf643bdc842e442
kroyo <kroyoshi@163.com>
2019-03-20 18:12:47 +0800

    Loader 、 plugin 学习完成

--- REANME.md
+++ REANME.md
@@ -35,3 +35,20 @@ npm i -D webpack@<version>
 # 安装最新体验版本
 npm i -D webpack@beta
 ```
+
+## Loader
+```javascript
+module: {
+  rules: [
+    {
+      // 用正则去匹配要用该 loader 转换的 CSS 文件
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader?minimize'],
+    }
+  ]
+}
+```
+ 如上配置告诉 Webpack 在遇到以 .css 结尾的文件时先使用 css-loader 读取 CSS 文件，再交给 style-loader 把 CSS 内容注入到 JavaScript 里。
+在配置 Loader 时需要*注意*的是：
+- use 属性的值需要是一个由 Loader 名称组成的数组，Loader 的执行顺序是由后到前的；
+- 每一个 Loader 都可以通过 URL querystring 的方式传入参数，例如 css-loader?minimize 中的 minimize 告诉 css-loader 要开启 CSS 压缩。
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   // JavaScript 执行入口文件
@@ -8,5 +9,27 @@ module.exports = {
     filename: 'bundle.js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        // 用正则去匹配要用该 loader 转换的 CSS 文件
+        test: /\.css$/, 
+        // 1.Loader 的执行顺序是由后到前每一个  
+        // 2.Loader 都可以通过 URL querystring 的方式传入参数   
+        // minimize  css-loader 1.00 以上 已remove
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ], 
+      }
+    ]
+  },
+  // plugins 属性来配置需要使用的插件列表的。
+  plugins: [
+    new MiniCssExtractPlugin({
+      // 从 .js 文件中提取出来的 .css 文件的名称
+      filename: `[name]_[contenthash:8].css`
+    })
+  ]
 }
