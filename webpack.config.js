9efbcf09f87388a1562702694fda9bccd7cf6a1c
kroyo <kroyoshi@163.com>
2019-03-28 18:07:21 +0800

    entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,12 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
+  context: path.resolve(__dirname, 'app'),
   // JavaScript 执行入口文件
   entry: './main.js',
   output: {
-    // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle.js',
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件   Chunk 的名称来区分输出的文件名 [name].js
+    filename: '[name][hash:8].js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist')
   },
